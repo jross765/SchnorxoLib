80178632326806663a83f53036ca4594bf998755
James Rossini <james.rossini@posteo.de>
2024-04-17 09:53:03 +0200

    - One new exception class centralized from JGnuCashLib(NTools) / JKMyMoneyLib(NTools) - A couple of mini-corrections

---
+++ schnorxolib-base/src/main/java/xyz/schnorxoborx/base/beanbase/MappingException.java
@@ -0,0 +1,17 @@
+package xyz.schnorxoborx.base.beanbase;
+
+public class MappingException extends RuntimeException {
+
+    private static final long serialVersionUID = -5988857879950695273L;
+    
+    // ---------------------------------------------------------------
+
+    public MappingException() {
+    		super();
+    }
+
+    public MappingException(String str) {
+    	super(str);
+    }
+
+}
